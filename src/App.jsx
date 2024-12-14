8b527902b48713da59b851d2d2271397a07145ee
danieleBattistelli <bomber81@gmail.com>
2024-12-14 17:02:41 +0100

    feat: bonus

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,36 @@ import posts from './data/data'
 
 function App() {
   const publiscedPosts = posts.filter(post => post.published);
+  const uniqueTags = new Set();
+  for (let i = 0; i < posts.length; i++) {
+    for (let j = 0; j < posts[i].tags.length;
+      j++) {
+      uniqueTags.add(posts[i].tags[j]);
+
+    }
+  }
+  const tagsArray = Array.from(uniqueTags);
+
   return (
     <>
       <div className='app-container'>
         <AppHeader />
         <main>
+          <div className="tags-section">
+            <h2>Tags Distinti</h2>
+            <ul>
+              {tagsArray.map(tag => (
+                <li key={tag} className="tag-item">{tag}</li>
+              ))}
+            </ul>
+          </div>
           {publiscedPosts.map(post => (
             <PostCard
               key={post.id}
               title={post.title}
               image={post.image}
               content={post.content}
+              tags={post.tags}
             />
           ))}
         </main>
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,18 @@
 import './PostCard.css';
 
-const PostCard = ({ title, image, content, tag }) => {
+const PostCard = ({ title, image, content, tags }) => {
+    const defaultImage = 'https://placehold.co/600x400';
+    const imagepath = image ? `/img/${image}` : defaultImage;
+
     return (
         <div className="post-card">
-            {image && <img src={image} alt={title} className='Post-Card-image' />};
+            <img src={imagepath} alt={title} className="post-card-image" />
             <div className="post-card-content">
                 <h2 className='post-card-title'>{title}</h2>
                 <p className='post-card-text'>{content}</p>
+                <div className="post-card-tags">
+                    {tags.map(tag => <span key={tag} className={`post-card-tag ${tag}`}>{tag}</span>)}
+                </div>
                 <button className="post-card-button">LEGGI DI PIU`</button>
             </div>
         </div>
